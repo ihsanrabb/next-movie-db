2f19dbdd817a84afaa305db5b871d5ff4ab34a64
ihsan <ihsanrabbani25@gmail.com>
2020-09-16 11:41:21 +0700

    next movie db app

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4a997e229b91ff42fba191190b79b892'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6ffd9faf-14ec-49df-8e1c-cdc76862b420'
+};
